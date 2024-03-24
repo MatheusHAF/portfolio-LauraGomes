08b394bfee1e6e92bd7b0961a0a1083f23788696
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-03-24 09:00:30 -0300

    .

--- src/components/Pages/Projetos.js
+++ src/components/Pages/Projetos.js
@@ -1,10 +1,9 @@
 import styles from "../styles_modules/Projeto.module.css"
 
-import img1 from "../../images/slides/cortina-de-palco-vermelho-para-teatro-cortina-de-cena-de-opera_107791-1552.avif"
-import img2 from "../../images/slides/depositphotos_5903137-stock-photo-actor-with-maks-in-a.jpg"
-import img3 from "../../images/slides/images.jpeg"
-import img4 from "../../images/slides/inedi_in_cena_3-q92e6i4qhsgybw5qk451rqi4urhrewjyxf9s9wvkhc.jpg"
-import img5 from "../../images/slides/teatro-portugues.jpg"
+import img1 from "../../images/slides/OKUTÁ/00 capa.CR2"
+import img2 from "../../images/slides/TRAGÉDIA DO REI CRISTOPHE/00 CAPA.JPG"
+import img3 from "../../images/slides/UMA NOITE/00 CAPA.jpg"
+
 
 // import function to register Swiper custom elements
 import { register } from 'swiper/element/bundle';
@@ -17,12 +16,23 @@ function Projetos() {
             <h1>Projetos</h1>
             <div className={styles.container}>
                 <h2>Teatro</h2>
-                <swiper-container navigation="true" slides-per-view="2" loop="true" css-mode="true">
-                    <swiper-slide><img src={img1} alt=""/></swiper-slide>
-                    <swiper-slide><img src={img2} alt=""/></swiper-slide>
-                    <swiper-slide><img src={img3} alt=""/></swiper-slide>
-                    <swiper-slide><img src={img4} alt=""/></swiper-slide>
-                    <swiper-slide><img src={img5} alt=""/></swiper-slide>
+                <swiper-container navigation="true" loop="true">
+                    <swiper-slide>
+                        <div className={styles.projeto_dov}>
+                            <img src={img1} alt="" />
+                            <p>OKUTÁ</p>
+                        </div>
+                    </swiper-slide>
+                    <swiper-slide><div className={styles.projeto_dov}>
+                        <img src={img2} alt="" />
+                        <p>REI CRISTOPHE</p>
+                    </div>
+                    </swiper-slide>
+                    <swiper-slide><div className={styles.projeto_dov}>
+                        <img src={img3} alt="" />
+                        <p>UMA NOITE</p>
+                    </div>
+                    </swiper-slide>
                 </swiper-container>
             </div>
         </div>
