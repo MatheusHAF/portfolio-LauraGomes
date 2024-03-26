63093d11034a2f85933a91472dd1b10c07449fe4
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-03-26 18:57:09 -0300

    .

--- src/components/Pages/Galeria.js
+++ src/components/Pages/Galeria.js
@@ -1,6 +1,6 @@
 import { ResponsiveMasonry } from "react-responsive-masonry";
 import Masonry from "react-responsive-masonry";
-import styles from "../styles_modules/Galeria.module.css"
+import styles from "../styles_modules/Galeria.module.css";
 import { useState } from 'react';
 
 const Galeria = ({ images }) => {
@@ -15,7 +15,7 @@ const Galeria = ({ images }) => {
         : images;
 
     return (
-        <div className={styles.container}>
+        <>
             <div className={styles.btns}>
                 <button onClick={() => setSelectedFilter('')}>Todos</button>
                 <button onClick={() => handleFilterClick('teatro')}>Teatro</button>
@@ -24,26 +24,32 @@ const Galeria = ({ images }) => {
                 <button onClick={() => handleFilterClick('influencer')}>Influencer</button>
                 {/* Adicione mais botões para outras opções de filtro */}
             </div>
-            <ResponsiveMasonry
-                columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
-                
-            >
-                <Masonry gutter="10px">
-                    {
-                        filteredImages.map((image, index) => (
-                            image.url && (
-                                <img
-                                    key={index}
-                                    src={image.url}
-                                    alt={image.alt}
-                                    style={{ width: "100%", display: "block" }}
-                                />
-                            )
-                        ))
-                    }
-                </Masonry>
-            </ResponsiveMasonry>
-        </div>
+            <div className={styles.container}>
+                <ResponsiveMasonry
+                    columnsCountBreakPoints={{ 350: 1, 750: 2, 900: 3 }}
+                >
+                    <Masonry gutter="10px">
+                        {
+                            filteredImages.map((image, index) => (
+                                image.url && (
+                                    <div key={index} className={styles.card}>
+                                        <img
+                                            src={image.url}
+                                            alt={image.alt}
+                                            className={styles.image}
+                                        />
+                                        <div className={styles.overlay}></div>
+                                        <div className={styles.text}>
+                                            {image.alt}
+                                        </div>
+                                    </div>
+                                )
+                            ))
+                        }
+                    </Masonry>
+                </ResponsiveMasonry>
+            </div>
+        </>
     );
 };
 
