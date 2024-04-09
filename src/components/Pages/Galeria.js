bb5bac6a70ab3fc5723531ff9931d1fe15d5b746
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-04-09 02:59:14 -0300

    .

--- src/components/Pages/Galeria.js
+++ src/components/Pages/Galeria.js
@@ -1,8 +1,7 @@
 import { ResponsiveMasonry } from "react-responsive-masonry";
 import Masonry from "react-responsive-masonry";
 import styles from "../styles_modules/Galeria.module.css";
-import { useState } from 'react';
-import { Link } from "react-router-dom";
+
 
 const Galeria = ({ images }) => {
 
--- src/components/Pages/Projetos.js
+++ src/components/Pages/Projetos.js
@@ -1,6 +1,15 @@
 function Projetos() {
-    return(
-        <div><h1>Projetos</h1></div>
+    return (
+        <div>
+            <h1>Projetos</h1>
+            <div>
+                <h2>Teatro</h2>
+                <div>
+                    <p>Sit tempor amet Lorem fugiat quis dolor ad amet do qui sint sint ad.</p>
+                    <container>imgs imgs imgs</container>
+                </div>
+            </div>
+        </div>
     );
 }
 export default Projetos
