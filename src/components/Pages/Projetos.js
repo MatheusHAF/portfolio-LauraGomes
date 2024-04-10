98a7cc08a4779028c18a77aa6e00992c9e084247
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-04-10 16:34:42 -0300

    .

--- src/components/Pages/Projetos.js
+++ src/components/Pages/Projetos.js
@@ -1,22 +1,24 @@
 import data from '../../../src/db.json'
 import styles from '../styles_modules/Projetos.module.css'
-console.log(data)
 function Projetos() {
     return (
-        <div className={styles.container}>
+        <>
+            <div className={styles.container}>
             <h1>Projetos</h1>
-
-            {data.projetos.map((item, index) => (
-                <details key={index}>
-                    <summary> {item.title}</summary>
-                    <p>
-                        <h4>{item.subtitle}</h4>
-                        {item.desc}
-                    </p>
-                    <div></div>
-                </details>
-            ))}
-        </div>
+                <div className={styles.div_projs}>
+                    {data.projetos.map((item, index) => (
+                        <details key={index}>
+                            <summary> {item.title}</summary>
+                            <p>
+                                <h4>{item.subtitle}</h4>
+                                {item.desc}
+                            </p>
+                            <div></div>
+                        </details>
+                    ))}
+                </div>
+            </div>
+        </>
     );
 }
 export default Projetos
