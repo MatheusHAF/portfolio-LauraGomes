9718436c12805c7f674916bd06d407774c7da97b
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-04-10 18:45:28 -0300

    .

--- src/components/Pages/Contatos.js
+++ src/components/Pages/Contatos.js
@@ -1,6 +1,38 @@
+import styles from '../styles_modules/Contato.module.css'
 function Contatos() {
-    return(
-        <h1>Contatos</h1>
+    return (
+        <div className={styles.container}>
+            <div className={styles.left}>
+                <h1>Titulo</h1>
+                <p>Magna esse ullamco eiusmod nulla dolore.</p>
+                <h3> Me siga nas redes sociais</h3>
+                <nav>Icones aqui</nav>
+            </div>
+            <div className={styles.right}>
+                <h1>Formulário de Contato</h1>
+                <form>
+                    <nav>
+                        <label>Nome: </label>
+                        <input type="text" />
+                    </nav>
+                    <nav>
+                        <label>Assunto: </label>
+                        <input type="text" />
+                    </nav>
+                    <nav>
+                        <label>Seu E-mail: </label>
+                        <input type="email" />
+                    </nav>
+                    <nav>
+                        <label>Mensagem: </label>
+                        <textarea cols="30" rows="10"></textarea>
+                    </nav>
+                    <nav>
+                        <input className={styles.submit} type="submit" value='Enviar E-mail'/>
+                    </nav>
+                </form>
+            </div>
+        </div>
     );
 }
 export default Contatos
