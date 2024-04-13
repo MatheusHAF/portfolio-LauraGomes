afdaf35a8f9f9f5f134c0db1a51818a0c286b923
Matheus H. A. de Francisco <matheushenrique612@gmail.com>
2024-04-13 12:29:07 -0300

    .

--- src/components/Pages/Projetos.js
+++ src/components/Pages/Projetos.js
@@ -1,6 +1,15 @@
 import data from '../../../src/db.json'
 import styles from '../styles_modules/Projetos.module.css'
+
+import img1 from '../../images/TEATRO/OKUTÁ/00 capa.jpg'
+import img2 from '../../images/TEATRO/TRAGÉDIA DO REI CRISTOPHE/DSCF5179.jpg'
+import img3 from '../../images/TEATRO/UMA NOITE/00 CAPA.jpg'
 function Projetos() {
+    const dbimgs = [
+        {title:'Okuta',image: {img1}},
+        {title:'Uma Noite',image: {img1}},
+        {title:'Cristhope',image: {img1}},
+    ]
     return (
         <>
             <div className={styles.container}>
@@ -13,7 +22,10 @@ function Projetos() {
                                 <h4>{item.subtitle}</h4>
                                 {item.desc}
                             </p>
-                            <div></div>
+                            <div className={styles.card}>
+                                <img src={img1} alt="" />
+                                <p>Titulo</p>
+                            </div>
                         </details>
                     ))}
                 </div>
